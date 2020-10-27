f03a6228ce3a4f61673e295ee0070c6e68d2333b
okrysiuk <oleksandr.krysiuk@gmail.com>
2020-10-27 16:24:31 +0200

    fourth commit

--- asyncAwait/asyncAvait.js
+++ asyncAwait/asyncAvait.js
@@ -5,15 +5,40 @@ const delay = (ms) => {
 //const url = "https://jsonplaceholder.typicode.com/todos";
 const url = "https://reqres.in/api/users?page=2";
 
-function fetchTodos() {
+/*function fetchTodos() {
   console.log("Fetch todo started");
-  return delay(2000).then(() => {
-    return fetch(url).then((res) => res.json());
-  });
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  return delay(2000)
+    .then(() => {
+      return fetch(url, {
+        method: "GET",
+        //body: JSON.stringify(body),
+        headers: headers,
+      });
+    })
+    .then((res) => res.json());
 }
 
 fetchTodos()
   .then((data) => {
-    console.log("Data: " + data);
+    console.log("Data: ", data);
   })
   .catch((e) => console.error(e));
+  */
+
+async function fetchAsyncTodos() {
+  console.log("Fetch todo started..");
+  try {
+    await delay(2000);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Data: ", data);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.log("Finally block");
+  }
+}
+fetchAsyncTodos().then(() => console.log("Async function return Promise!"));
